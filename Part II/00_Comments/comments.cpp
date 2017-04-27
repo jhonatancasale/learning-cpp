7389f81e54c4928874990a9cb5a3a773006e739d
jhonatancasale <jhonatan@jhonatancasale.com>
2017-04-27 11:58:19 -0300

    Add content about comments

---
+++ Part II/00_Comments/comments.cpp
@@ -0,0 +1,27 @@
+/*
+ * Author: Jhonatan Casale (jhc)
+ *
+ * Contact : jhonatan@jhonatancasale.com
+ *         : casale.jhon@gmail.com
+ *         : https://github.com/jhonatancasale
+ *         : https://twitter.com/jhonatancasale
+ *         : http://jhonatancasale.github.io/
+ *
+ * Create date Thu 27 Apr 11:56:58 -03 2017
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+  // one line comment
+
+  /*
+   * multiline
+   * comment
+   */
+
+  return (EXIT_SUCCESS);
+}

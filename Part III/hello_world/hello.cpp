5245b01f9596006f2b1e7d25275f8bf27882c313
jhonatancasale <jhonatan@jhonatancasale.com>
2017-05-09 20:14:34 -0300

    Add initial hello world example

---
+++ Part III/hello_world/hello.cpp
@@ -0,0 +1,21 @@
+/*
+ * Author: Jhonatan Casale (jhc)
+ *
+ * Contact : jhonatan@jhonatancasale.com
+ *         : casale.jhon@gmail.com
+ *         : https://github.com/jhonatancasale
+ *         : https://twitter.com/jhonatancasale
+ *         : http://jhonatancasale.github.io/
+ *
+ * Create date Tue  9 May 20:12:25 -03 2017
+ *
+ */
+
+#include <iostream>
+
+int main (int argc, char **argv)
+{
+  std::cout << "Hello, world" << std::endl;
+
+  return (0);
+}
